ef5f8c4c26a506c249fac7c9fcb9dda8b6e64ddb
NudeDude <HateSpirit666@gmail.com>
2017-12-16 22:06:28 +0100

    initial commit

--- app/src/main/java/org/nuclearfog/twidda/newwindow/Profile.java
+++ app/src/main/java/org/nuclearfog/twidda/newwindow/Profile.java
@@ -2,19 +2,36 @@ package org.nuclearfog.twidda.newwindow;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import org.nuclearfog.twidda.R;
+import org.nuclearfog.twidda.engine.TwitterEngine;
 
 public class Profile extends AppCompatActivity {
+
+    private ListView homeTl;
+    private TabHost mtab;
+    private TextView username, bio,link,following;
+    private ImageView profile_img, profile_banner;
+    private SwipeRefreshLayout refresh;
+
 
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.profile);
+        initElements();
+        initTabs();
+        initSwipe();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu m) {
         getMenuInflater().inflate(R.menu.buttons, m);
@@ -37,6 +54,53 @@ public class Profile extends AppCompatActivity {
     }
 
 
+    private void initTabs(){
+        mtab = (TabHost)findViewById(R.id.tabhost);
+        mtab.setup();
+        // Tab #1
+        TabHost.TabSpec tab1 = mtab.newTabSpec("tweets");
+        tab1.setIndicator("Tweets").setContent(R.id.home_tl);
+        mtab.addTab(tab1);
+        // Tab #2
+        TabHost.TabSpec tab2 = mtab.newTabSpec("favorites");
+        tab1.setIndicator("Favorits").setContent(R.id.home_tl);
+        mtab.addTab(tab2);
+        // Listener
+        mtab.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) { }
+        });
+    }
 
+    private void loadContent(){}
+
+    private void initHomeTimeline(){
+
+
+    }
+
+    /**
+     * Profile Contents
+     */
+    private void initElements() {
+        homeTl = (ListView) findViewById(R.id.home_tl);
+        username = (TextView) findViewById(R.id.my_username);
+        bio = (TextView) findViewById(R.id.bio);
+        link = (TextView) findViewById(R.id.links);
+        following = (TextView) findViewById(R.id.follow);
+        profile_img = (ImageView) findViewById(R.id.profile_img);
+        profile_banner = (ImageView) findViewById(R.id.banner);
+    }
+
+    /**
+     * Swipe Refresh Layout
+     */
+    private void initSwipe(){
+        refresh = (SwipeRefreshLayout) findViewById(R.id.refresh);
+        refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() { }
+        });
+    }
 
 }
