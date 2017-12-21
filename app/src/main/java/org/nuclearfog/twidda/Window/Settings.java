72dc61dcc58c3520119fcfac85b69c2ac96deba8
NudeDude <HateSpirit666@gmail.com>
2017-12-21 21:06:21 +0100

    Major Enhancements

--- app/src/main/java/org/nuclearfog/twidda/Window/Settings.java
+++ app/src/main/java/org/nuclearfog/twidda/Window/Settings.java
@@ -1,12 +1,10 @@
 package org.nuclearfog.twidda.Window;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 
 import org.nuclearfog.twidda.R;
 
