2f0124fe1a6848446b0a49a4f24f10ce60f7872e
nuclearfog <hatespirit666@gmail.com>
2022-05-01 12:56:18 +0200

    added new license dialog

--- app/src/main/java/org/nuclearfog/twidda/ui/dialogs/LicenseDialog.java
+++ app/src/main/java/org/nuclearfog/twidda/ui/dialogs/LicenseDialog.java
@@ -2,8 +2,7 @@ package org.nuclearfog.twidda.ui.dialogs;
 
 import android.app.Dialog;
 import android.content.Context;
-
-import com.larswerkman.licenseview.LicenseView;
+import android.webkit.WebView;
 
 import org.nuclearfog.twidda.R;
 
@@ -14,19 +13,13 @@ import org.nuclearfog.twidda.R;
  */
 public class LicenseDialog extends Dialog {
 
-    /**
-     *
-     */
+
     public LicenseDialog(Context context) {
         super(context, R.style.LicenseDialog);
-        setContentView(R.layout.dialog_licenses);
-        LicenseView licenseView = findViewById(R.id.license_view);
-        try {
-            licenseView.setLicenses(R.xml.licenses);
-        } catch (Exception err) {
-            err.printStackTrace();
-            dismiss();
-        }
+        WebView htmlViewer = new WebView(context);
+        setContentView(htmlViewer);
+
+        htmlViewer.loadUrl("file:///android_asset/licenses.html");
     }
 
 
